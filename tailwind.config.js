a050b3571b6edbf6688b261674e39a416d8910b7
sachin <sk800968@gmail.com>
2023-08-03 16:26:05 +0530

    Discord Project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+
+    fontFamily:{
+      abc:['IM Fell Double Pica', 'serif'],
+      abc2:['Roboto', 'sans-serif'],
+      abc3:['Sacramento', 'cursive'],
+      abc4:['Noto Sans', 'sans-serif'],
+    },
+    
+  },
+  plugins: [],
+}
